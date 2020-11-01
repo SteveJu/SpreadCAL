d23c14ce6e46aa628ab87c020437bb417daeaec6
Steve Ju <steveju546@gmail.com>
2020-10-31 22:21:27 -0400

    Version 2

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-# Author: Steve Zhu
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from scipy.ndimage.filters import convolve as imfilter
+from skimage.color import rgb2gray
 import cv2
+from skimage import morphology
+from skimage.morphology import skeletonize
+from skimage import measure
+
+Img = rgb2gray(plt.imread('test_img/template.jpg'))
+cv2.imshow('img', Img)
+
+# Exiting the window if 'q' is pressed on the keyboard.
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
