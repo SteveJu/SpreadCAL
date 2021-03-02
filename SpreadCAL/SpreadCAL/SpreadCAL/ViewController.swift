e64015ff693182047845afb1a46ab4a270e4456b
Steve Ju <steveju546@gmail.com>
2021-03-01 23:25:41 -0500

    Add files via upload

---
+++ SpreadCAL/SpreadCAL/SpreadCAL/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  SpreadCAL
+//
+//  Created by Steve Ju on 1/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        button.backgroundColor = .systemRed
+        button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+    }
+    
+    @IBAction func didTapButton(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+
+}
+extension ViewController:UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate{
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated:true,completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as?
+                UIImage else{
+            return
+        }
+        imageView.image = image
+    }
+}
